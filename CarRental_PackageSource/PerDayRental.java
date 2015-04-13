ae5a84fcea2d6bf99b4a6e3093ce2c75aa329ef6
Simon Lehmann <slehmann36@gmail.com>
2015-04-13 18:05:57 +0800

    Added PerDayRental.java
    Added PerDayRental.java

---
+++ CarRental_PackageSource/PerDayRental.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Simon Lehmann
+ */
+public class PerDayRental {
+        
+    private double perDayKm;
+    private final int CHARGE_RATE_DAY = 100;
+    private double totalCost;
+    private int daysRented;
+    
+    private Journey getDistance;
+    
+    PerDayRental(int daysRented){
+        perDayKm = getDistance.getKilometers();
+        daysRented =  this.daysRented;
+    }
+    
+    double computeDays() {
+        totalCost = CHARGE_RATE_DAY * daysRented;
+        return totalCost;
+    }
+}
